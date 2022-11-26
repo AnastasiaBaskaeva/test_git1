4167f2d6e622bdd3580ba67a92748609b82bea53
Anastasia <bnastua10.2006@gmail.com>
2022-11-26 17:27:49 +0300

    colour_for_console

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 import requests
 import webbrowser
+from colorama import *
+init()
 url = 'https://anime777.ru/api/rand'
 response=requests.get(url)
-genre=input("Введите жанр (Enter если не важно)")
+genre=input(Fore.YELLOW + "Введите жанр (Enter если не важно)")
 year=input("Введите год выпуска (Enter если не важно)")
-episodes=input("Введите количество эпизодов (Enter если не важно)")
+episodes=input("Введите количество эпизодов (Enter если не важно)" + Style.RESET_ALL)
 choise={"genre":genre,
         "year": year,
         "episodes_total": episodes}
 response.raise_for_status()
 response=requests.get(url, params=choise)
-print(response.json())
-print('Название:',response.json()['title'])
-print("Озвучка:",response.json()['translate'])
-print("Кол-во эпизодов",response.json()['material_data']['episodes_total'])
-print("Год выпуска:",response.json()['year'])
-print("Описание",response.json()['material_data']['description'])
-print("Жанры:",response.json()['material_data']['all_genres'])
+print(Fore.MAGENTA + 'Название:' + Style.RESET_ALL,response.json()['title'])
+print(Fore.MAGENTA +"Озвучка:" + Style.RESET_ALL,response.json()['translate'])
+print(Fore.CYAN +"Кол-во эпизодов:" + Style.RESET_ALL,response.json()['material_data']['episodes_total'])
+print(Fore.CYAN +"Год выпуска:" + Style.RESET_ALL,response.json()['year'])
+print(Fore.MAGENTA +"Описание:" + Style.RESET_ALL,response.json()['material_data']['description'])
+print(Fore.CYAN +"Жанры:" + Style.RESET_ALL,response.json()['material_data']['all_genres'])
 webbrowser.open(response.json()['material_data']["poster_url"])
