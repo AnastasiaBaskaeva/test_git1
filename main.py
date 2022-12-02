a3444900d18c4040a7f912be2d2937adf5b63c20
Anastasia <bnastua10.2006@gmail.com>
2022-12-02 18:15:53 +0300

    Revert "correcting main code"
    This reverts commit 93a19e3a6fa5a3f45359efef9dd4785d5475321c.

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import requests
 import webbrowser
-<<<<<<< Updated upstream
 from colorama import *
 init()
 def run():
@@ -32,22 +31,3 @@ def save_history(response):
                 print("__________________________________________", file=f)
 run()
 input("Enter чтобы закрыть")
-=======
-url = 'https://anime777.ru/api/rand'
-response=requests.get(url)
-genre=input("Введите жанр (Enter если не важно)")
-year=input("Введите год выпуска (Enter если не важно)")
-episodes=input("Введите количество эпизодов (Enter если не важно)")
-choise={"genre":genre,
-        "year": year,
-        "episodes_total": episodes}
-response.raise_for_status()
-response=requests.get(url, params=choise)
-print('Название:',response.json()['title'])
-print("Озвучка:",response.json()['translate'])
-print("Кол-во эпизодов:",response.json()['material_data']['episodes_total'])
-print("Год выпуска:",response.json()['year'])
-print("Описание:",response.json()['material_data']['description'])
-print("Жанры:",response.json()['material_data']['all_genres'])
-webbrowser.open(response.json()['material_data']["poster_url"])
->>>>>>> Stashed changes
