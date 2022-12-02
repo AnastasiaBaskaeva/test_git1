b3560c77773bc8105932ff07f9d23bc7da85ea3c
Anastasia <bnastua10.2006@gmail.com>
2022-12-02 20:02:06 +0300

    save to fav + colour

--- main.py
+++ main.py
@@ -1,33 +1,65 @@
 import requests
 import webbrowser
 from colorama import *
+# https://anime777.ru/api/rand?genre=%D0%A1%D0%BF%D0%BE%D1%80%D1%82
 init()
+a = Fore.MAGENTA
+b = Fore.YELLOW
+c = Fore.CYAN
+d=Fore.RESET
+print(a + '\n\t\t' "Добро пожаловать!")
+print(a)
+
 def run():
+
         url = 'https://anime777.ru/api/rand'
-        response=requests.get(url)
-        choise={"genre":input(Fore.YELLOW + "Введите жанр (Enter если не важно)"),
-                "year": input("Введите год выпуска (Enter если не важно)"),
-                "episodes_total": input("Введите количество эпизодов (Enter если не важно)" + Style.RESET_ALL)}
+        response = requests.get(url)
+        choise = {"genre": input(b + "Введите жанр (Enter если не важно)" + d),
+                  "year": input(b + "Введите год выпуска (Enter если не важно)" + d),
+                  "episodes_total" : input(b + "Введите количество эпизодов (Enter если не важно)" + d)}
         response.raise_for_status()
-        response=requests.get(url, params=choise)
+        response = requests.get(url, params=choise)
         print_request(response)
         save_history(response)
+        save_to_favourites(response)
+
 def print_request(response):
-        print(Fore.MAGENTA + 'Название:' + Style.RESET_ALL,response.json()['title'])
-        print(Fore.MAGENTA +"Озвучка:" + Style.RESET_ALL,response.json()['translate'])
-        print(Fore.CYAN +"Кол-во эпизодов:" + Style.RESET_ALL,response.json()['material_data']['episodes_total'])
-        print(Fore.CYAN +"Год выпуска:" + Style.RESET_ALL,response.json()['year'])
-        print(Fore.MAGENTA +"Описание:" + Style.RESET_ALL,response.json()['material_data']['description'])
-        print(Fore.CYAN +"Жанры:" + Style.RESET_ALL,response.json()['material_data']['all_genres'])
+
+        print(c + 'Название:' + d, a, response.json()['title'])
+        print(c + "Озвучка:" + d, a, response.json()['translate'])
+        print(c + "Кол-во эпизодов:" + d, a, response.json()['material_data']['episodes_total'])
+        print(c + "Год выпуска:" + d, a, response.json()['year'])
+        print(c + "Описание:" + d, a, response.json()['material_data']['description'])
+        print(c + "Жанры:" + d, a, response.json()['material_data']['all_genres'])
         webbrowser.open(response.json()['material_data']["poster_url"])
+
 def save_history(response):
+
         with open("history.txt", "a") as f:
-                print('Название:', response.json()['title'], file=f)
-                print("Озвучка:", response.json()['translate'], file=f)
                 print("Кол-во эпизодов:", response.json()['material_data']['episodes_total'], file=f)
-                print("Год выпуска:", response.json()['year'], file=f)
+                print("Год выпуска:",  response.json()['translate'], file=f)
+                print('Название:', response.json()['title'], file=f)
+                print("Озвучка:", response.json()['year'], file=f)
                 print("Описание:", response.json()['material_data']['description'], file=f)
                 print("Жанры:", response.json()['material_data']['all_genres'], file=f)
                 print("__________________________________________", file=f)
+def save_to_favourites(response):
+        favourite = input(b + "Добавить в избранное? да/нет")
+        if favourite == "да":
+                with open("favourite.txt", "a") as h:
+                        print('Название:', response.json()['title'], file=h)
+                        print("Озвучка:", response.json()['translate'], file=h)
+                        print("Кол-во эпизодов:", response.json()['material_data']['episodes_total'], file=h)
+                        print("Год выпуска:", response.json()['year'], file=h)
+                        print("Описание:", response.json()['material_data']['description'], file=h)
+                        print("Жанры:", response.json()['material_data']['all_genres'], file=h)
+                        print("__________________________________________", file=h)
 run()
-input("Enter чтобы закрыть")
+while True:
+        ask = (b + input("Хотите продолжить? да/нет" + a))
+        if ask == "да":
+                run()
+        else:
+                print(a + '\n\t\t', "Хорошего дня!")
+                input(b + "Enter чтобы закрыть" + a)
+                break
